78db488bf13c7fc5ed3620171334eedd4bf52266
jonahmarc <olympusfoxtrot@gmail.com>
2022-10-21 11:20:23 +0800

    messages scroll  start at bottom

--- client/src/components/chatroom/messages/message-history.component.jsx
+++ client/src/components/chatroom/messages/message-history.component.jsx
@@ -1,7 +1,8 @@
 import { Stack } from 'react-bootstrap';
 
-import './message-history.component.scss';
+import { useEffect, useRef } from 'react';
 
+import './message-history.component.scss';
 
 const data = []
 
@@ -13,8 +14,56 @@ export const onMessageReceived = (payload) =>{
 }
 
 const Messages = () => {
+
+    const messages = useRef();
+
+    const scrollToBottom = () => {
+        const scroll =
+        messages.current.scrollHeight -
+        messages.current.clientHeight;
+        messages.current.scrollTo(0, scroll);
+    };
+
+    useEffect (() => {
+        scrollToBottom()
+    });
+
     return (
-        <div className='messages_history mt-2 p-0 d-flex flex-column'>
+        <div ref={messages} className='messages_history mt-2 p-0 d-flex flex-column'>
+            <div className="received align-self-start ms-2 mb-3 p-2">
+                <p className='m-0 p-0 fs-6 fw-lighter'><em>iandave</em></p>
+                <p className='m-0 p-0 fs-6 fw-lighter'><em>10/12/22 17:37</em></p>
+                <p className='m-0 p-0'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Repellat quasi dicta repudiandae velit excepturi! 
+                    Beatae vel excepturi ipsum odio assumenda facere ipsa unde dolore consequatur fugiat. 
+                    Maxime nobis adipisci dicta.
+                </p>
+            </div>
+            <div className="sent align-self-end me-2 mb-3 p-2">
+                <p className='m-0 p-0 fs-6 fw-lighter'><em>10/12/22 18:13</em></p>
+                <p className='m-0 p-0'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Repellat quasi dicta repudiandae velit excepturi! 
+                    Beatae vel excepturi ipsum odio assumenda facere ipsa unde dolore consequatur fugiat. 
+                    Maxime nobis adipisci dicta.
+                </p>
+            </div>
+            <div className="received align-self-start ms-2 mb-3 p-2">
+                <p className='m-0 p-0 fs-6 fw-lighter'><em>iandave</em></p>
+                <p className='m-0 p-0 fs-6 fw-lighter'><em>10/12/22 17:37</em></p>
+                <p className='m-0 p-0'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Repellat quasi dicta repudiandae velit excepturi! 
+                    Beatae vel excepturi ipsum odio assumenda facere ipsa unde dolore consequatur fugiat. 
+                    Maxime nobis adipisci dicta.
+                </p>
+            </div>
+            <div className="sent align-self-end me-2 mb-3 p-2">
+                <p className='m-0 p-0 fs-6 fw-lighter'><em>10/12/22 18:13</em></p>
+                <p className='m-0 p-0'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Repellat quasi dicta repudiandae velit excepturi! 
+                    Beatae vel excepturi ipsum odio assumenda facere ipsa unde dolore consequatur fugiat. 
+                    Maxime nobis adipisci dicta.
+                </p>
+            </div>
             <div className="received align-self-start ms-2 mb-3 p-2">
                 <p className='m-0 p-0 fs-6 fw-lighter'><em>iandave</em></p>
                 <p className='m-0 p-0 fs-6 fw-lighter'><em>10/12/22 17:37</em></p>
